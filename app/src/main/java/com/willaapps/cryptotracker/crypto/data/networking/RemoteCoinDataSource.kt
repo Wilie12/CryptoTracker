480fcbe0fc689f6e0e1c1dcf981dea3caeff4c86
Wilie12 <albertw536@gmail.com>
2024-11-05 21:00:44 +0100

    Implemented functionality to fetch Coin data

--- app/src/main/java/com/willaapps/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
+++ app/src/main/java/com/willaapps/cryptotracker/crypto/data/networking/RemoteCoinDataSource.kt
@@ -6,11 +6,17 @@ import com.willaapps.cryptotracker.core.domain.util.NetworkError
 import com.willaapps.cryptotracker.core.domain.util.Result
 import com.willaapps.cryptotracker.core.domain.util.map
 import com.willaapps.cryptotracker.crypto.data.mappers.toCoin
+import com.willaapps.cryptotracker.crypto.data.mappers.toCoinPrice
+import com.willaapps.cryptotracker.crypto.data.networking.dto.CoinHistoryDto
 import com.willaapps.cryptotracker.crypto.data.networking.dto.CoinsResponseDto
 import com.willaapps.cryptotracker.crypto.domain.Coin
 import com.willaapps.cryptotracker.crypto.domain.CoinDataSource
+import com.willaapps.cryptotracker.crypto.domain.CoinPrice
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 class RemoteCoinDataSource(
     private val client: HttpClient
@@ -23,6 +29,33 @@ class RemoteCoinDataSource(
             )
         }.map { response ->
             response.data.map { it.toCoin() }
+        }
+    }
+
+    override suspend fun getCoinHistory(
+        coinId: String,
+        start: ZonedDateTime,
+        end: ZonedDateTime
+    ): Result<List<CoinPrice>, NetworkError> {
+        val startMillis = start
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+        val endMillis = end
+            .withZoneSameInstant(ZoneId.of("UTC"))
+            .toInstant()
+            .toEpochMilli()
+
+        return safeCall<CoinHistoryDto> {
+            client.get(
+                urlString = constructUrl("/assets/$coinId/history")
+            ) {
+                parameter("interval", "h6")
+                parameter("start", startMillis)
+                parameter("end", endMillis)
+            }
+        }.map { response ->
+            response.data.map { it.toCoinPrice() }
         }
     }
 }
